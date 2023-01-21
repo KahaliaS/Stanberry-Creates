a34fed4b75a6269ace371c90de55351a1a8e770a
Kahalia Stanberry <kahalia.stanberry@gmail.com>
2023-01-21 19:26:56 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+
+### Help For This Project:
+Service Card: https://codepen.io/ericmahoney/pen/oNLMOYw
+
+Team Section: https://youtu.be/EOpt0HLymis
---
+++ js/app.js
@@ -0,0 +1,9 @@
+// function navSelector(){
+//   const navSelect = document.querySelector('#nav-items');
+//   const listItem = document.createElement("li");
+//   let app = navSelect.append(listItem)
+//   app.append('Home');
+  
+// }
+
+// navSelector()
